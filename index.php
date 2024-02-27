752ba6a1e319805d5611a4fba084f38aeda52e15
ashein-codethedreams <heroglasses155@gmail.com>
2024-02-27 14:51:31 +0630

    for loop basic

--- index.php
+++ index.php
@@ -7,10 +7,18 @@ $name ="Ashein";
 function showAge(){
     global $name; //declaration of global var in local
  $age=24;
- echo "age of ".$name." is ".$age;
+ echo "age of ".$name." is ".$age. "<br>";
 }
 showAge();
 
 //in php , local block also doesn't know global variables
+
+function sayMyName($initial){
+    echo "$initial Aye Myat Mon<br>";
+}
+for($i=0;$i<10;$i++){
+    sayMyName($i);
+}
+
 ?>
 
