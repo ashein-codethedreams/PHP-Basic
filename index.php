2dfff39ae4a486b1da1b254d4a425b16c1accaad
ashein-codethedreams <heroglasses155@gmail.com>
2024-03-19 10:29:11 +0630

    php class

--- index.php
+++ index.php
@@ -1,29 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-    $employees = [
-        ["name" => "John", "employeeNo" => 001, "role" => "Admin"],
-        ["name" => "Alex", "employeeNo" => 002, "role" => "Staff"],
-        ["name" => "Mike", "employeeNo" => 003, "role" => "Staff"]
-    ];
-
-    foreach ($employees as $employee) {
-        if ($employee['role'] == "Admin") {
-    ?>
-            <h1><?php echo $employee['name'] ?></h1>
-            <h1> His emplyoee number is <?php echo $employee['employeeNo'] . "." ?></h1>
-    <?php
-        }
+//class
+class Car
+{
+    private $name;
+    function __construct($carName)
+    {
+        $this->name = $carName;
     }
-    ?>
-</body>
+    function drive()
+    {           //method
+        echo $this->name . " is driving...";
+    }
+}
 
-</html>
+//object
+$carObj = new Car("Lamborghini");
+$carObj->drive();
